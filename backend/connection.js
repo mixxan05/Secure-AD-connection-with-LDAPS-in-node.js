0476531e9fbd91c080866d92c33b985f834e11cf
Michael Kaiser <92426393+mixxan05@users.noreply.github.com>
2023-11-27 13:04:51 +0100

    Update connection.js

--- backend/connection.js
+++ backend/connection.js
@@ -1,35 +1,63 @@
 const express = require('express');
+const session = require('express-session');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+const ActiveDirectory = require('activedirectory');
+const CryptoJS = require("crypto-js"); //Verschlüsselung
 
-// CORS aktivieren (vor den Routen)
-app.use(cors()); //hierbei werden Anfragen aus allen Routen erlaubt, ist in einer Produktionsumgebung nicht sicher!
-// Deshalb: 
-// Konfiguration für CORS
-//const corsOptions = {
-    //origin: 'http://localhost:8080', // Erlaubt nur Anfragen von dieser Adresse
-   //methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Erlaubt alle HTTP-Methoden
-    //credentials: true, // Erlaubt das Senden von Cookies über Cross-Origin-Anfragen
-    //optionsSuccessStatus: 204, // Ein spezieller Statuscode für Preflight-Anfragen
-//};
-
-app.use(bodyParser.json());
-
+app.use(session);
 app.post('/login', (req, res) => {
+
     const enteredPassword = req.body.password;
-    const correctPassword = 'Test';
+    const enteredUsername = req.body.username;
+    req.session.username = enteredUsername;
+    req.session.password = enteredPassword;
+    const encryptP = req.body.password;
+    const encryptU = req.body.username; //verschlüsselt
+    const passphrase = req.body.t1;
+    const bytesP = CryptoJS.AES.decrypt(encryptP, passphrase);
+    const bytesU = CryptoJS.AES.decrypt(encryptU, passphrase);
+    const decryptedP = bytesP.toString(CryptoJS.enc.Utf8);
+    const decryptedU = bytesU.toString(CryptoJS.enc.Utf8); //entschlüsselt
+    user = decryptedU;
+    function AbgleichAD(decryptedP, decryptedU) {
+        var activeDirectory = new ActiveDirectory({
+            url: global.URL,
+            baseDN: global.baseDN,
+            username: global.usernameValue,
+            password: global.passwordValue,
+            tlsOptions: {
+                rejectUnauthorized: true,
+            },
+        });
+        let authenticationAttempted = false;
+        activeDirectory.authenticate(decryptedU, decryptedP, (error) => {
 
-    console.log('Anfrage zum Login erhalten. Passwort:', enteredPassword);
+            if (authenticationAttempted) {
+                return;
+            }
+            authenticationAttempted = true;
 
-    if (enteredPassword === correctPassword) {
-        res.status(200).json({ message: 'Login erfolgreich' });
-    } else {
-        res.status(401).json({ message: 'Falsches Passwort' });
+            if (error) {
+                console.log('Fehlgeschlagener Login. Username:', decryptedU, 'Zeit:', new Date().toLocaleString());
+                console.log('ERROR: ' + JSON.stringify(error));
+                return res.status(401).json({ success: false, message: 'Falsches Passwort' });
+            } else {
+
+                loggedIn = true;
+                isAuthenticated = true;
+                console.log('Erfolgreicher Login. Username:', decryptedU, 'Zeit:', new Date().toLocaleString());
+                return res.status(200).json({ success: true, message: 'Login erfolgreich', user: decryptedU });
+            }
+            next();
+        });
     }
-});
 
+    AbgleichAD(decryptedP, decryptedU);
+
+});
 app.listen(port, () => {
     console.log(`Server läuft auf Port ${port}`);
 });
